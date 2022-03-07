f80d37103b6b06439bbc0b9c383d04dc2a5d160b
arthur sanchez <arthur@zonda.claus.fr>
2022-03-07 23:17:13 +0100

    jourmagique start

--- jourmagiques.py
+++ jourmagiques.py
@@ -1,1 +1,15 @@
-def joursmagiques():
+lsemaine=['samedi','dimanche','lundi','mardi','mercredi','jeudi','vendredi']
+lmois=['janvier','fevrier','mars','avril','mai','juin','juillet','août','septembre'
+          ,'octobre','novembre','décembre']
+lannée=[2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,
+           ,2015,2016,2017,2018,2019,2020,2021,2022]
+ljours=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27
+           ,28,29,30,31]
+nbr_jours_magiques=0
+
+def calendrier(D,F):
+    while D!=F:
+
+
+def jours+1(D): #D est la liste (lundi,7,mars,2022)
+    jour=(D[1]+1)%31
